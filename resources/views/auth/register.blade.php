1cfd9cfef8133f048b0fc00363c237635c02b322
cGabi69 <gabriel.costa@ynov.com>
2023-04-14 18:35:03 +0200

    test

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+<head>
+<link rel="stylesheet" href="/auth/login.css" /> 
+</head>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
